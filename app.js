cddf1de3fd020877c4290b7ed671cd8439dd2e75
igordg777 <52235357+igordg777@users.noreply.github.com>
2021-12-14 09:33:11 +0300

    расширяет окно при вводе текста

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-function speak (text){
-    const message = new SpeechSynthesisUtterance();
-    message.lang = 'ru-RU';
-    message.text = text;
-    window.speechSynthesis.speak(message);
+message.addEventListener("keyup", function () {
+  if (this.scrollTop > 0) {
+    this.style.height = this.scrollHeight + "px";
+  }
+});
+
+function speak(text) {
+  const message = new SpeechSynthesisUtterance();
+  message.lang = "ru-RU";
+  message.text = text;
+  window.speechSynthesis.speak(message);
 }
 
-const btn = document.getElementById('speak');
-const txtMessage = document.getElementById('message');
-btn.addEventListener('click', () => {
-    speak(txtMessage.value);
+const btn = document.getElementById("speak");
+const txtMessage = document.getElementById("message");
+const btnStop = document.getElementById("speakStop");
+btn.addEventListener("click", () => {
+  speak(txtMessage.value);
+});
+
+let flag = true;
+
+btnStop.addEventListener("click", () => {
+  window.speechSynthesis.cancel();
 });
